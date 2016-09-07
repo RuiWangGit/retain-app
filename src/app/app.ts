7316d6d75647e7a7c671a33bab8a4510da2c02c8
Rui Wang <wangr3@gmail.com>
2016-09-07 14:29:07 -0700

    1 - Yo World

---
+++ src/app/app.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app',
+    template: `
+        <div>
+            <h3>
+                    tes;tsre;
+            </h3>
+
+        </div>
+    `
+})
+
+export class App {
+
+}
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { bootstrap } from '@angular/platform-browser-dynamic';
+
+
+import { App } from './app/app';
+
+
+
+bootstrap(App);
